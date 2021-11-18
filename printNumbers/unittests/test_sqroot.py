0d70194041f47c7764b2b7148f24ace4d09a3aa4
p.dunkel@fz-juelich.de <p.dunkel@fz-juelich.de>
2021-11-18 12:59:15 +0100

    added function for root

---
+++ printNumbers/unittests/test_sqroot.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# test_sqroot.py
+#
+# This file is part of PrintNumbers.
+#
+# Copyright (C) 2017 G. Trensch, Simulation & Datalab Neuroscience, JSC, FZ Juelich
+#
+# PrintNumbers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# PrintNumbers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PrintNumbers.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Unit tests: 'square_root'.
+#
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import unittest
+from functions.square_root import *
+
+class TestSquareRoot(unittest.TestCase):
+
+    def test_value_0(self):
+        self.assertEqual(SquareRoot(0), 0)
+
+    def test_value_1(self):
+        self.assertEqual(SquareRoot(1), 1)
+
+    def test_value_2(self):
+        self.assertEqual(SquareRoot(4), 2)
+
+    def test_value_20(self):
+        self.assertEqual(SquareRoot(64), 8)
+
+
+def suite():
+    suite = unittest.makeSuite(SquareRoot, 'test')
+    return suite
+
+def run():
+    runner = unittest.TextTestRunner(verbosity = 2)
+    runner.run(suite())
+
+if __name__ == "__main__":
+    run()
